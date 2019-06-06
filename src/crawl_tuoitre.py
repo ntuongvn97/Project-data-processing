20a26ecb8c87634342f93774a87fcd0ccbc11f30
Fitznguyen <ntuongvn97@gmail.com>
2019-06-06 15:22:22 +0700

    crawl data Hacker news

---
+++ src/crawl_tuoitre.py
@@ -0,0 +1,35 @@
+import scrapy
+import re
+from pprint import pprint
+# array_field = ["draft", "STT", "NUMBER_PLATE",
+# "TYPE_OF_VEHICLE", "TYPE_OF_VIOLATION", "DATE", "ADDRESS"]
+# data = {}
+
+
+class BrickSetSpider(scrapy.Spider):
+    name = "article"
+    domain = "https://news.ycombinator.com/news"
+    start_urls = ['https://news.ycombinator.com/news']
+
+    def parse(self, response):
+        f = open("data/article.txt", "a")
+        SET_SELECTOR = 'table .itemlist'
+        body_mid = response.css(SET_SELECTOR)
+        data_title = body_mid.css('tr.athing')
+        data_info = body_mid.css('tr td.subtext')
+        for i, data in enumerate(data_title):
+            title = data.css('td.title a.storylink::text').get()
+            author = data_info[i].css('a.hnuser::text').get()
+            d = "{}, {}".format(title, author)
+            f.write(d + "\n")
+        import time
+        time.sleep(3)
+        f.close()
+        NEXT_PAGE = "a.morelink::attr(href)"
+        body = response.css(NEXT_PAGE).extract_first()
+        print(response.css(NEXT_PAGE))
+        if body:
+            yield scrapy.Request(
+                response.urljoin(body),
+                callback=self.parse
+            )
